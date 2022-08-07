21b9417ecd5b0a3afb9af20a0eb5f3a3f464e262
ANTONIO FERREIRA DE OLIVEIRA JUNIOR <antonio-250291@hotmail.com>
2022-08-06 21:03:45 -0300

    Feat corrigindo erro thema

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,12 @@
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
+interface DefaultThemeProps {
+  colors: string[]
+}
+
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme<DefaultThemeProps> extends ThemeType {}
 }
